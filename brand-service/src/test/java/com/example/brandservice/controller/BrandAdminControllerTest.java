42682a4dcfadd00e8ec1417d8f5e21092d965e14
whwp4151 <whwp4151@naver.com>
2023-07-25 20:03:19 +0900

    fix: restdocs 브랜드 계정 생성 API 추가

--- brand-service/src/test/java/com/example/brandservice/controller/BrandAdminControllerTest.java
+++ brand-service/src/test/java/com/example/brandservice/controller/BrandAdminControllerTest.java
@@ -7,9 +7,13 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.brandservice.dto.BrandAccountRequestDto;
 import com.example.brandservice.dto.BrandRequestDto;
 import com.google.gson.Gson;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,12 +27,14 @@ import org.springframework.test.web.servlet.ResultActions;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BrandAdminControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @Order(1)
     public void createBrand() throws Exception {
         //given
         BrandRequestDto brandRequestDto = BrandRequestDto.builder()
@@ -63,6 +69,52 @@ class BrandAdminControllerTest {
                     fieldWithPath("data.depositAmount").type(JsonFieldType.NUMBER).description("예치금 금액"),
                     fieldWithPath("data.adminId").description("브랜드 담당자"),
                     fieldWithPath("data.isActive").type(JsonFieldType.BOOLEAN).description("브랜드 활성화 여부")
+                )
+            ));
+    }
+
+    @Test
+    @Order(2)
+    public void createBrandAccount() throws Exception {
+        //given
+        BrandAccountRequestDto requestDto = BrandAccountRequestDto.builder()
+            .loginId("test")
+            .password("1234")
+            .brandId(1L)
+            .accountNumber("1122")
+            .bankName("은행명")
+            .holderName("이체자명")
+            .build();
+
+        String content = new Gson().toJson(requestDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(RestDocumentationRequestBuilders.post("/admin/brandAccount")
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions.andExpect(status().isOk())
+            .andDo(print())
+            .andDo(document("create-brand-account",
+                requestFields(
+                    fieldWithPath("loginId").description("로그인 아이디"),
+                    fieldWithPath("password").description("비밀번호"),
+                    fieldWithPath("brandId").description("브랜드"),
+                    fieldWithPath("accountNumber").description("계좌 번호"),
+                    fieldWithPath("bankName").description("은행 이름"),
+                    fieldWithPath("holderName").description("예치금 예치자 이름")
+                ),
+                responseFields(
+                    fieldWithPath("code").description("결과코드"),
+                    fieldWithPath("message").description("메시지"),
+                    fieldWithPath("data").description("데이터"),
+                    fieldWithPath("data.brandAccountId").type(JsonFieldType.NUMBER).description("브랜드 계정 고유번호"),
+                    fieldWithPath("data.loginId").description("로그인 아이디"),
+                    fieldWithPath("data.bankInfo").description("은행 정보"),
+                    fieldWithPath("data.bankInfo.accountNumber").description("계좌 번호"),
+                    fieldWithPath("data.bankInfo.bankName").description("은행 이름"),
+                    fieldWithPath("data.bankInfo.holderName").description("예치금 예치자 이름")
                 )
             ));
     }

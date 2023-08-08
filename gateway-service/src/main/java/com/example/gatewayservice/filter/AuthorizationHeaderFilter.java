fcfc068e3a71e7916acd0d8926d56a7aee8d3953
whwp4151 <whwp4151@naver.com>
2023-08-08 20:14:56 +0900

    fix: gateway 필터 수정

--- gateway-service/src/main/java/com/example/gatewayservice/filter/AuthorizationHeaderFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -2,13 +2,11 @@ package com.example.gatewayservice.filter;
 
 import com.example.gatewayservice.filter.AuthorizationHeaderFilter.Config;
 import com.example.gatewayservice.jwt.JwtTokenProvider;
-import io.jsonwebtoken.Jwts;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -21,12 +19,19 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Config> {
 
-    private final String secret;
+    private final String userSecret;
+    private final String brandSecret;
+    private final String adminSecret;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthorizationHeaderFilter(@Value("${token.secret}") String secret, JwtTokenProvider jwtTokenProvider) {
+    public AuthorizationHeaderFilter(@Value("${token.secret}") String userSecret,
+                                    @Value("${brand.secret}") String brandSecret,
+                                    @Value("${admin.secret}") String adminSecret,
+                                    JwtTokenProvider jwtTokenProvider) {
         super(Config.class);
-        this.secret = secret;
+        this.userSecret = userSecret;
+        this.brandSecret = brandSecret;
+        this.adminSecret = adminSecret;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
@@ -42,7 +47,17 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Conf
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
 
-            if (!jwtTokenProvider.isJwtValid(jwt, secret)) {
+            String secret = null;
+
+            if (config.isHasUserRole() && jwtTokenProvider.isJwtValid(jwt, userSecret)) {
+                secret = userSecret;
+            } else if (config.isHasBrandRole() && jwtTokenProvider.isJwtValid(jwt, brandSecret)) {
+                secret = brandSecret;
+            } else if (config.isHasAdminRole() && jwtTokenProvider.isJwtValid(jwt, adminSecret)) {
+                secret = adminSecret;
+            }
+
+            if (secret == null) {
                 return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
             }
 
@@ -66,8 +81,11 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Conf
         return response.setComplete();
     }
 
+    @Getter
     public static class Config {
-
+        private boolean hasUserRole;
+        private boolean hasBrandRole;
+        private boolean hasAdminRole;
     }
 
 }

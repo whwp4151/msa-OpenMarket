22823925181b9a0301559cb0d46cf9f3778afda9
whwp4151 <whwp4151@naver.com>
2023-08-29 21:05:33 +0900

    fix: payment 작업중

--- order-service/src/main/java/com/example/orderservice/domain/Payment.java
+++ order-service/src/main/java/com/example/orderservice/domain/Payment.java
@@ -2,8 +2,11 @@ package com.example.orderservice.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +14,17 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Payment {
+public class Payment extends BaseEntity {
 
     @Id
     @GeneratedValue
     @Column(name = "payment_id")
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer amount;
 
 }
--- order-service/src/main/java/com/example/orderservice/domain/ProductInfo.java
+++ order-service/src/main/java/com/example/orderservice/domain/ProductInfo.java
@@ -22,4 +22,6 @@ public class ProductInfo {
 
     private Integer productPrice;
 
+    private Integer productVersion;
+
 }
